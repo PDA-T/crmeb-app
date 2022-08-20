2b4c5bb8329ebdd98d13f74d84457d7a4d651d82
PDA <96939164+PDA-T@users.noreply.github.com>
2022-08-20 16:35:11 +0800

    Update app.js
    更改h5

--- config/app.js
+++ config/app.js
@@ -1,12 +1,14 @@
 
-let domain = 'http://127.0.0.1:20101'
+// Crmeb-foron服务地址
+let domain = 'http://127.0.0.1:8081'
+// let domain = 'http://43.142.191.46:8080'
 
 module.exports = {
 	// 请求域名 格式： https://您的域名
 	// #ifdef MP
 		HTTP_REQUEST_URL: domain,
 	// #endif
-	HTTP_ADMIN_URL:'xxx.xxx.xxx', //PC后台的API请求地址，上传图片用
+	HTTP_ADMIN_URL:'http://127.0.0.1:8080', //PC后台的API请求地址，上传图片用(Crmeb-admin服务地址)
 	// #ifdef H5
 		//H5接口是浏览器地址
 		// HTTP_REQUEST_URL: window.location.protocol+"//"+window.location.host,
